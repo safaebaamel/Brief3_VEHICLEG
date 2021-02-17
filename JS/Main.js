2d48e4fca3d0c3d1c31dcc6a777dac8a0a810960
Sapae Safae <sapaebaamel@gmail.com>
2021-02-17 21:23:22 +0100

    update

--- JS/Main.js
+++ JS/Main.js
@@ -1,48 +1,48 @@
-// //slider
-// var slideIndex = 1;
-// showSlides(slideIndex);
+//slider
+var slideIndex = 1;
+showSlides(slideIndex);
 
-// function plusSlides(x) {
+function plusSlides(x) {
 
-//   showSlides(slideIndex += x);
-// }
-
-
-// function showSlides(x) {
-//   var i;
-//   var slides = document.getElementsByClassName("ImageSlide"); //T
-//   if (x > slides.length) {
-//     slideIndex = 1
-//   }
-//   if (x < 1) {
-//     slideIndex = slides.length
-//   }
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   // display
-//   slides[slideIndex - 1].style.display = "block";
+  showSlides(slideIndex += x);
+}
 
 
+function showSlides(x) {
+  var i;
+  var slides = document.getElementsByClassName("ImageSlide"); //T
+  if (x > slides.length) {
+    slideIndex = 1
+  }
+  if (x < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  // display
+  slides[slideIndex - 1].style.display = "block";
 
-//   //slide image car reservation
-//   var myIndex = 0;
-//   carousel();
 
-//   function carousel() {
-//     var i;
-//     var x = document.getElementsByClassName("slid");
-//     for (i = 0; i < x.length; i++) {
-//       x[i].style.display = "none";
-//     }
-//     myIndex++;
-//     if (myIndex > x.length) {
-//       myIndex = 1
-//     }
-//     x[myIndex - 1].style.display = "block";
-//     setTimeout(carousel, 2500);
-//   }
-// }
+
+  //slide image car reservation
+  var myIndex = 0;
+  carousel();
+
+  function carousel() {
+    var i;
+    var x = document.getElementsByClassName("slid");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {
+      myIndex = 1
+    }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 2500);
+  }
+}
 
 // prompt contact us page
 function MessageChannelbox() {
@@ -164,9 +164,11 @@ function reservation() {
   if (i < 0) {
     alert("Please enter a valid number greater than or equal to 1.");
   }
-  else {
+  else if (i >= 1) {
     som = som * i;
     alert("Thank you for your purchase\n" +  
     "The bill: "+ som + "\$");
+  } else {
+    alert("Please enter a number");
   }
 }
